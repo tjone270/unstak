b5e3cefd937ea9cb1a0b834b6e41892fd357be3d
hyperwired <hyperwired@gg>
2016-08-23 22:48:33 +1000

    Adding search markers, embedding licence

--- balancer.py
+++ balancer.py
@@ -1,7 +1,30 @@
-#----------------------------------------------------------------------------------------------------------------------------------------
+# UNSTAK_START ---------------------------------------------------------------------------------------------------------
+#
 # unstak, an alternative balancing method for minqlx created by github/hyperwired aka "stakz", 2016-07-31
-# This plugin is released to everyone, for any purpose. It comes with no warranty, no guarantee it works, it's released AS IS.
-# You can modify everything, except for lines 1-4. They're there to indicate I whacked this together originally. Please make it better :D
+# This code is released under the MIT Licence:
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2016
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ----------------------------------------------------------------------------------------------------------------------
 
 import bisect
 import collections
@@ -377,6 +400,4 @@ def generate_switch_proposals(teams, verbose=False, max_results=5):
 
     return switch_proposals
 
-
-# end unstak
-#----------------------------------------------------------------------------------------------------------------------------------------
+# UNSTAK_END -----------------------------------------------------------------------------------------------------------
